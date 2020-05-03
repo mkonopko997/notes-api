d0abdb08ce62f6c6ad81ee1e948bbb6ddba6ecdf
Michal Konopko <m.konopko@storware.eu>
2020-05-03 22:16:34 +0200

    master - cors disabled

--- src/main/java/com/example/notes/auth/WebSecurityConfig.java
+++ src/main/java/com/example/notes/auth/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and().csrf().disable()
 // dont authenticate this particular request
                 .authorizeRequests().antMatchers("/auth", "/user").permitAll().
 // all other requests need to be authenticated
